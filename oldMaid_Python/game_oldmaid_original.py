881a9c847b045facbe2020dac76f37992d505d7b
Atsushi Fujita <atsushi24f@gmail.com>
2021-01-10 13:01:10 +0900

    oldMaid game create deck

---
+++ oldMaid_Python/game_oldmaid_original.py
@@ -0,0 +1,14 @@
+print('ババ抜き')
+
+# joker入りdeck
+# 
+
+import random
+
+# joker入りdeck
+trump = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * 4
+trump.append('joker')
+
+print(trump)
+
+
