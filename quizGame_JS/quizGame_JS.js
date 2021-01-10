601ecb45ea3080c47e7515dd5a2c9b8bbb23bf29
Atsushi Fujita <atsushi24f@gmail.com>
2021-01-10 12:52:53 +0900

    refactoring ( button and 正誤判定 )

--- quizGame_JS/quizGame_JS.js
+++ quizGame_JS/quizGame_JS.js
@@ -8,42 +8,41 @@ const answers = [
 ];
 const correct = 'キャンベラ';
 
-  //Id追加しHTMLに反映,QUESTIONと紐づけ
-  //text contentを変更
-document.getElementById('ind-question').textContent = question;
-  //buttonと答えの紐づけ,HTML上のtagを変数にする場合は$を付ける！
-  const $button = document.getElementsByTagName('button');
-    $button[0].textContent = answers[0];
-    $button[1].textContent = answers[1];
-    $button[2].textContent = answers[2];
-    $button[3].textContent = answers[3];
+//buttonの配列をdocumentを利用して変数として設定
+const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-  //eventの追加,正誤判定
-$button[0].addEventListener('click', function () {
-  if ($button[0].textContent === correct) {
+//問題とボタンの定義,リファクタリング
+const setupQuiz = function () {
+  //Id追加しHTMLに反映,QUESTIONと紐づけ
+  //textcontentを変更
+  document.getElementById('ind-question').textContent = question;
+  //buttonと答えの紐づけ,HTML上のtagを変数にする場合は$を付ける！
+  //ループ文loop
+  let buttonIndex = 0;
+    while (buttonIndex < buttonLength) {　//lengthは配列個数 //PCが壊れるため必ず上限を設定
+      //繰り返したい命令
+      $button[buttonIndex].textContent = answers[buttonIndex];
+      buttonIndex++; //indexに+1し続ける
+    }
+}
+setupQuiz();
+
+//if文を関数にまとめる
+const clickhandler =　function (event) {
+  if (event.target.textContent === correct) {
     window.alert('正解！！');
   }else {
     window.alert('不正解...');
   }
-});
-$button[1].addEventListener('click', function () {
-  if ($button[1].textContent === correct) {
-    window.alert('正解!!');
-  }else {
-    window.alert('不正解...');
-  }
-});
-$button[2].addEventListener('click', function () {
-  if ($button[2].textContent === correct) {
-    window.alert('正解!!');
-  }else {
-    window.alert('不正解...');
-  }
-});
-$button[3].addEventListener('click', function () {
-  if ($button[3].textContent === correct) {
-    window.alert('正解!!');
-  }else {
-    window.alert('不正解...');
-  }
-});
+}
+
+//buttonの正誤判定をリファクタリング
+let handleIndex = 0;
+while (handleIndex < buttonLength) {　//lengthは配列個数 //PCが壊れるため必ず上限を設定
+  //繰り返したい命令
+  $button[handleIndex].addEventListener('click', function (event) {
+   clickhandler(event);
+  });
+  handleIndex++; //indexに+1し続ける
+}
